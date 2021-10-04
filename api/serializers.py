c13631023169a60d950473ab60c81cf8b567ff6d
Fuukss <rafal19fuchs@gmail.com>
2021-10-04 10:29:56 +0200

    Added view with serializer included validation to upload images

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,20 @@
+import os
+import sys
+import tempfile
+from sys import getsizeof
+from cv2 import imread, cv2
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from rest_framework import serializers
 
 from image.models import ImagePost
 
 
 class ImageSerializer(serializers.ModelSerializer):
-    '''
-    After added context parameter in views image is the absolute path of file
-    '''
-    # photo_url = serializers.SerializerMethodField()
-
     username = serializers.SerializerMethodField('get_username_from_author')
+    image = serializers.SerializerMethodField('validate_image_url')
+
     class Meta:
         model = ImagePost
         fields = ['image', 'slug', 'username']
@@ -18,7 +23,58 @@ class ImageSerializer(serializers.ModelSerializer):
         username = image_post.author.username
         return username
 
-    # def get_photo_url(self, ImagePost):
-    #     request = self.context.get('request')
-    #     photo_url = ImagePost.image.url
-    #     return request.build_absolute_uri(photo_url)
+    def validate_image_url(self, image_post):
+        image = image_post.image
+        new_url = image.url
+        if "?" in new_url:
+            new_url = image.url[:image.url.rfind("?")]
+        return new_url
+
+
+IMAGE_SIZE_MAX_BYTES = 20000000
+
+
+class ImagePostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImagePost
+        fields = ['image', 'slug', 'author']
+
+    def save(self):
+
+        try:
+            image = self.validated_data['image']
+            slug = self.validated_data['slug']
+
+            image_post = ImagePost(
+                author=self.validated_data['author'],
+                slug=slug,
+                image=image,
+            )
+
+            url = os.path.join(tempfile.gettempdir(), str(image))
+            storage = FileSystemStorage(location=url)
+
+            with storage.open('', 'wb+') as destination:
+                for chunk in image.chunks():
+                    destination.write(chunk)
+                destination.close()
+
+            if sys.getsizeof(image.file) > IMAGE_SIZE_MAX_BYTES:
+                os.remove(url)
+                raise serializers.ValidationError(
+                    {"response": "That image is too large. Images must be less than 2 MB. Try a different image."})
+
+            img = cv2.imread(url)
+            dimensions = img.shape  # gives: (height, width, ?)
+
+            aspect_ratio = dimensions[1] / dimensions[0]  # divide w / h
+            if aspect_ratio < 1:
+                os.remove(url)
+                raise serializers.ValidationError(
+                    {"response": "Image height must not exceed image width. Try a different image."})
+
+            os.remove(url)
+            image_post.save()
+            return image_post
+        except KeyError:
+            raise serializers.ValidationError({"response": "You must have a slug and an image."})
--- api/views.py
+++ api/views.py
@@ -9,7 +9,9 @@ from rest_framework import generics
 
 from account.models import Account
 from image.models import ImagePost
-from api.serializers import ImageSerializer
+from api.serializers import ImageSerializer, ImagePostCreateSerializer
+
+CREATE_SUCCESS = 'created'
 
 
 @api_view(['GET', ])
@@ -24,27 +26,8 @@ def api_detail_image_view(request, slug):
         serializer = ImageSerializer(image_post, context={'request': request})
         return Response(serializer.data)
 
-
-@api_view(['POST', ])
-@permission_classes((IsAuthenticated,))
-def api_create_image_view(request):
-    """
-    This view should post a image
-    for the currently authenticated user
-    and response details.
-    """
-    account = request.user
-
-    image_post = ImagePost(author=account)
-
-    if request.method == "POST":
-        serializer = ImageSerializer(image_post, context={'request': request}, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+# Url: https://<your-domain>/api/blog/list
+# Headers: Authorization: Token <token>
 @permission_classes((IsAuthenticated,))
 class ApiImageListView(generics.ListAPIView):
     queryset = ImagePost.objects.all()
@@ -60,3 +43,28 @@ class ApiImageListView(generics.ListAPIView):
         """
         user = self.request.user
         return ImagePost.objects.filter(author=user)
+
+
+# Url: https://<your-domain>/api/blog/create
+# Headers: Authorization: Token <token>
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def api_create_image_view(request):
+    if request.method == 'POST':
+
+        data = request.data
+        data['author'] = request.user.pk
+        serializer = ImagePostCreateSerializer(data=data)
+
+        data = {}
+        if serializer.is_valid():
+            image_post = serializer.save()
+            data['response'] = CREATE_SUCCESS
+            data['slug'] = image_post.slug
+            image_url = str(request.build_absolute_uri(image_post.image.url))
+            if "?" in image_url:
+                image_url = image_url[:image_url.rfind("?")]
+            data['image'] = image_url
+            data['username'] = image_post.author.username
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
